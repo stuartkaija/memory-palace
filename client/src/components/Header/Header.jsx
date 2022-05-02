ef0f76a33b688f5736136336a91be3e612e70506
Stuart Kaija <stuartkaija@gmail.com>
2022-05-02 13:34:30 -0700

    continuing on styling (fitting cards on page)

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import './Header.scss';
 
 export default function Header({ turns, points, setModalIsOpen, shuffleCards }) {
-  return (
-      <>
-        <h1 className='title'>Memory Palace</h1>
-        <div className='game-details'>
-            <h4 className='game-details__turns'>Turns: {turns}</h4>
-            <button className='game-details__button' onClick={() => {setModalIsOpen(true)}}>New Game</button>
-            <h4 className='game-details__matches'>Points: {points}</h4>
-        </div>
-      </>
-  )
+	return (
+		<>
+			<div className='title-container'>
+				<h1 className='title'>Memory Palace</h1>
+			</div>
+			<div className='game-details'>
+				<h4 className='game-details__turns'>Turns: {turns}</h4>
+				<button className='game-details__button' onClick={() => {setModalIsOpen(true)}}>New Game</button>
+				<h4 className='game-details__matches'>Points: {points}</h4>
+			</div>
+		</>
+	)
 }
